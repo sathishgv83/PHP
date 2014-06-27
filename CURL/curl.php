30257beed5107f3acbe85a1a5ee366336dccc770
sathishgv83 <sathish.gv83@gmail.com>
2014-06-27 18:32:18 +0530

    Removed unwanted codes

--- CURL/curl.php
+++ CURL/curl.php
@@ -28,18 +28,6 @@ $.ajax({
         alert("error: "+data+" status: "+status+" er:"+er);
     }
 });
-	/*  var jsonString = "{\"name\":\"hmkcode\",\"id\":2}";
-	  $.ajax({ 
-          url:"/SpringJsonJackson/rest/cont/person",    
-          type:"POST", 
-          contentType: "application/json; charset=utf-8",
-          mimeType: 'application/json',
-          data: jsonString, //Stringified Json Object
-          async: false,    //Cross-domain requests and dataType: "jsonp" requests do not support synchronous operation
-          cache: false,    //This will force requested pages not to be cached by the browser          
-          processData:false, //To avoid making query String instead of JSON
-          success: function(resposeJsonObject){                           
-  }}); */
 }
 </script>
 </head>
